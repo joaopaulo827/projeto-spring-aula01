ebee12f9e49c96a4643cef4beb34798177c3d6f3
joaopaulo827 <joaopauloterziotti@gmail.com>
2026-04-08 17:32:18 -0300

    SpringBoot2

---
+++ src/main/java/com/aulaum/main/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aulaum.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Aluno
+ */
+@Controller
+public class UsuarioController {
+    @GetMapping("/perfil")
+    public String perfil(Model model) {
+        model.addAttribute("nome","João Paulo");
+        model.addAttribute("curso","Desenvolvimento");
+        model.addAttribute("LingyagemFavorita","Matematica");
+        return "perfil";
+    }
+}
